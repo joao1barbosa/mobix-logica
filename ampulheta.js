47cf26922b17b81a5b9feda39524c498b23cbfcd
joao1-barbosa <joao.rbarbosa3@gmail.com>
2022-11-17 14:31:18 -0300

    new refactoring

--- ampulheta.js
+++ ampulheta.js
@@ -1,96 +1,88 @@
 const timePerLine = [];
-let time;
+let totalTime;
 let timer;
 
-//A função verificar se o valor passado pelo usuário é válido (número inteiro e maior que 5) e apresenta mensagem com o erro correspondente
 function isNumber(num){
-   if (Number(num) === parseInt(num)){
-    if(parseInt(num) < 5){
-        console.log("Error: Não é possível construir uma Ampulheta com este tamanho!");
-        return false;
-    }else{
-        return parseInt(num);
-    }
-   }else{
-    console.log("Error: Valor digitado é inválido!");
-    return false;
-   }
+    return !(isNaN(Number(num)));
 }
 
-//Função que enumera a quantidade de "grãos" por linha e o total de grãos
-function controlTimePerLine(size){
-    for(i=(size-4);i>=0; i-= 2){
+function isValidValue(num){
+    return isNumber(num) && (num >= 5); 
+}
+
+function countTimePerLine(size){
+    for(i = (size - 4); i >= 0; i -= 2){
         timePerLine.push(i);
     }
 
-    time = timePerLine.reduce(function(total, acc){
+    countTotalTime();    
+}
+
+function countTotalTime(){
+    totalTime = timePerLine.reduce(function(total, acc){
         return total+acc;
     });
 }
 
-function timerControl(size){ //Função que controla a queda dos "grãos" a cada intervalo de tempo
-    if(!(size === false)){
+function timerControl(size){
+    countTimePerLine(size);
 
-        controlTimePerLine(size);
-
-        timer = setInterval(function () {
-            console.clear();
-            drawHourglass(size);
-        }, 300); //intervalo de tempo (em milisegundos) para cada "grão" cair
-    }
+    timer = setInterval(function () {
+        console.clear();
+        drawHourglass(size);
+    }, 300);
 }
 
-//Função que desenha a linha superior e inferior da Ampulheta
 function drawEndLine(size){
     let line = "";
-    for(i=0;i<size;i++){
+    
+    for(i = 0; i < size; i++){
         line += "-";
     }
 
     console.log(line);
 }
 
-//Função que desenha a Ampulheta
 function drawHourglass(size){
 
         console.log("n = " + size);
-        drawEndLine(size);//Desenha linha superior
-        let cont = 0;
+
+        drawEndLine(size);
+        let count = 0;
     
-        let cornerWidth = 0; //Largura entre a moldura da ampulheta e sua lateral 
-        let middleWidth = size - 4; //Largura interna da ampulheta
-        let cornerWidthLine;//String com o espaço equivalente à variável cornerWidth
+        let cornerWidth = 0;
+        let withinWidth = size - 4; 
         
-        while(middleWidth >= 0){ //Repete o bloco do código até a largura interna da Ampulheta não existir (parte superior até o meio exato da Ampulheta)
+        let cornerWidthLine;
+        
+        while(withinWidth >= 0){
             
             cornerWidthLine = "";
-            for(i=0;i<cornerWidth; i++){//Define qual será o espaço entre a moldura da Ampulheta e sua lateral nesta linha
+            for(i = 0; i < cornerWidth; i++){
                 cornerWidthLine += " ";
             }
     
             let line = "";
-            let j = 0;
+            let count2 = 0;
 
-            while(j<middleWidth){//Define qual será o espaço interno da Ampulheta nesta linha
-                for(k=0; k<timePerLine[cont]; k++) { // 
+            while(count2 < withinWidth){
+                for(k = 0; k < timePerLine[count]; k++) { // 
                     line += "*";
-                    j++;
+                    count2++;
                 }
-                for(k=0; j<middleWidth; k++){
+                for(k = 0; count2 < withinWidth; k++){
                     line += " ";
-                    j++;
+                    count2++;
                 }
             }
     
-            line = "|" + cornerWidthLine + "\\" + line + "/" + cornerWidthLine + "|"; //Monta como será a linha atual da Ampulheta
+            line = "|" + cornerWidthLine + "\\" + line + "/" + cornerWidthLine + "|";
     
-            console.log(line);//Desenha a linha atual da Ampulheta
+            console.log(line);
 
-    
-            //Contadores dos tamanhos dos espaços na ampulheta
+            withinWidth -= 2;
             cornerWidth++; 
-            middleWidth -= 2;
-            cont++;
+            count++;
         }
         
         //Cria uma linha a mais na Ampulheta se o tamanho for ímpar (apenas uma melhoria visual);
@@ -99,36 +91,37 @@ function drawHourglass(size){
         }
     
         
-        while(cornerWidth >= 1){//Desenha a parte inferior da Ampulheta 
-            //Contadores dos tamanhos dos espaços na ampulheta
+        while(cornerWidth >= 1){
+            withinWidth += 2;
             cornerWidth--;
-            middleWidth += 2;
-            cont--;
+            count--;
     
             cornerWidthLine = "";
-            for(i=cornerWidth;i > 0; i--){//Define qual será o espaço entre a moldura da Ampulheta e sua lateral nesta linha
+            for(i = cornerWidth; i > 0; i--){
                 cornerWidthLine += " ";
             }
     
             let line = "";
-            let j = 0;
+            let count2 = 0;
             
-            while(j<middleWidth){//Define qual será o espaço interno da Ampulheta nesta linha
-                for(k=0; k<timePerLine[cont]; k++) { // 
+            while(count2 < withinWidth){
+                for(j=0; j < timePerLine[count]; j++) { // 
                     line += " ";
-                    j++;
+                    count2++;
                 }
-                for(k=0; j<middleWidth; k++){
+                for(j=0; count2 < withinWidth; j++){
                     line += "*";
-                    j++;
+                    count2++;
                 }
             }
     
-            line = "|" + cornerWidthLine + "/" + line + "\\" + cornerWidthLine + "|";//Monta como será a linha atual da Ampulheta
+            line = "|" + cornerWidthLine + "/" + line + "\\" + cornerWidthLine + "|";
     
-            console.log(line);//Desenha a linha atual da Ampulheta
+            console.log(line);
         }
 
+        drawEndLine(size);
+        
         //Este bloco decrementa a quantidade de grãos restantes 
         contDecres = 0;
         while(true){
@@ -139,17 +132,20 @@ function drawHourglass(size){
                 break;
             }
         }
-    
-        drawEndLine(size); //Desenha linha inferior
 
-        time--;
-        if(time === -1){
+        totalTime--;
+        if(totalTime === -1){
             clearInterval(timer);
         }
 }
 
+function startHourglass(test, size){
+    if (test) return timerControl(size);
+
+    console.log("Valor inválido ou muito pequeno para montar a Ampulheta!");
+}
 
 const readLineSync = require("readline-sync");
 const n = readLineSync.question("Digite o tamanho da Ampulheta: "); 
 
-timerControl(isNumber(n));
+startHourglass(isValidValue(n), n);
